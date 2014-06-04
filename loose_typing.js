09fbee88d832841987f0ff8def4d24cce25aa16e
Pepe Doval <pepellou@gmail.com>
2014-06-04 13:52:40 +0200

    Another good part is loose typing
    JS does not check types for you, thus allowing a cleaner expressive
    code and avoiding typing syntax sugar (declaring types, casting, etc.)

---
+++ loose_typing.js
@@ -0,0 +1,10 @@
+function inc(x) {
+    return x + 1;
+}
+
+document.writeln(inc(41));
+document.writeln(inc(inc(40)));
+document.writeln(inc('x'));
+document.writeln(inc());
+document.writeln(inc(inc));
+document.writeln(inc(inc(inc)));
