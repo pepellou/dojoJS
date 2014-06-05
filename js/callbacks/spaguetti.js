f0c151047cd3916aa6d9513e23acff6d844dd317
Pepe Doval <pepellou@gmail.com>
2014-06-05 12:59:52 +0200

    Simplified example for spaguetti code
    I don't have time to build promises right now. We'll solve it in the dojo :-)

--- js/callbacks/spaguetti.js
+++ js/callbacks/spaguetti.js
@@ -3,17 +3,18 @@ $(function() {
     $.ajax({
         url: 'https://api.github.com/users/' + username + '/repos',
         success: function(repos) {
-            repos.forEach(function(repo) {
-                $.ajax({
-                    url: repo.contributors_url,
-                    success: function(users) {
-                        users.forEach(function(user) {
-                            if (user.login != username) {
-                                $('pre').append('<p>' + user.login + '</p>');
-                            }
-                        });
-                    }
-                });
+            var firstRepo = repos[0];
+            $.ajax({
+                url: firstRepo.events_url,
+                success: function(events) {
+                    var firstEvent = events[0];
+                    $.ajax({
+                        url: firstEvent.payload.commits[0].url,
+                        success: function(payload) {
+                            $('pre').append('<p>Last commit message in repo ' + firstRepo.name + ' is <strong>' + payload.commit.message + '</strong>.</p>');
+                        }
+                    });
+                }
             });
         }
     });
